07627a4fa35dfc58f2e3a1d79819b26773135f0d
rolantin <maksimtianblue@hotmail.com>
2018-10-21 17:58:15 +0800

    update

--- vs2017_project/opengl/opengl/Triangle.cpp
+++ vs2017_project/opengl/opengl/Triangle.cpp
@@ -5,18 +5,34 @@ using namespace std;
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 void framerbuffer_size_callback(GLFWwindow * window, int width, int height);
+void processInput(GLFWwindow *window);
 
 
-const char *vertexShaderSource = "#version 330 core\n";
-const char *fragmentShaderSource = "#version 300 core\n";
+//定义顶点位置 与初始位置
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//定义输出位置与最终颜色
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 
 int main() {
+	//初始化glfw
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//创建窗体
 	GLFWwindow*window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"Traingle", NULL, NULL);
 	if (window == NULL) {
 		cout << "fail to creat glfw window" << endl;
@@ -24,32 +40,34 @@ int main() {
 		return -1;
 	}
 
-	glfwMakeContextCurrent(window);
-
+	//创建上下文
+	glfwMakeContextCurrent(window);	
+	//回调函数 窗口大小
 	glfwSetFramebufferSizeCallback(window, framerbuffer_size_callback);
 
+	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		cout << "failed to initialize glad" << endl;
 		return -1;
 	}
 	//----------------------- VERTEX --------------------------------------------------
-
-	// build and compile our shader program
-	// ------------------------------------
-	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource函数把要编译的着色器对象作为第一个参数。第二参数指定了传递的源码字符串数量，这里只有一个。第三个参数是顶点着色器真正的源码，第四个参数我们先设置为NULL。
 	//我们把需要创建的着色器类型以参数形式提供给glCreateShader
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//付到着色器上 glShaderSource函数把要编译的着色器对象作为第一个参数。第二参数指定了传递的源码字符串数量，这里只有一个。第三个参数是顶点着色器真正的源码，第四个参数我们先设置为NULL。
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//编译顶点着色器
 	glCompileShader(vertexShader);
 
+	//----------------------- 判断是否编译成功 --------------------------------------------------
 	//首先我们定义一个整型变量来表示是否成功编译，还定义了一个储存错误消息（如果有的话）的容器。然后我们用glGetShaderiv检查是否编译成功。
 	//如果编译失败，我们会用glGetShaderInfoLog获取错误消息，然后打印它。
 	int success;
-	char infoLog[512];
+	char infoLog[512]; //字符串
 
-	//get info and print it 
+	//用glGetShaderiv检查是否编译成功
 	glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
 	if (!success) {
+		//glGetShaderInfoLog 会获取 glGetShaderiv的信息
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		cout << "error shader vertex compilation faild \n" << endl;
 	}
@@ -83,16 +101,18 @@ int main() {
 
 
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+	  -0.5f, -0.5f, 0.0f, // left  
+	   0.5f, -0.5f, 0.0f, // right 
+	   0.0f,  0.5f, 0.0f  // top   
 	};
 
+	//定义四边形 使用
 	unsigned int indices[] = {
 		0, 1, 3,  // first Triangle
 		1, 2, 3   // second Triangle
 	};
+
+
 	//顶点数组对象(Vertex Array Object, VAO)可以像顶点缓冲对象那样被绑定，任何随后的顶点属性调用都会储存在这个VAO中。
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
@@ -108,9 +128,8 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//使用glVertexAttribPointer函数告诉OpenGL该如何解析顶点数据（应用到逐个顶点属性上） . 设置顶点属性指针
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-	//第一个参数指定我们要配置的顶点属性。还记得我们在顶点着色器中使用layout(location = 0)定义了position顶点属性的位置值(Location)吗？
-	//它可以把顶点属性的位置值设置为0。因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
+	glVertexAttribPointer(0 , 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	//第一个参数指定我们要配置的顶点属性。还记得我们在顶点着色器中使用layout(location = 0)定义了position顶点属性的位置值(Location)吗？它可以把顶点属性的位置值设置为0。因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
 	//第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
 	//第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
 	//第四个参数参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是 - 1）到1之间。我们把它设置为GL_FALSE。
@@ -120,23 +139,58 @@ int main() {
 	//每个顶点属性从一个VBO管理的内存中获得它的数据，而具体是从哪个VBO（程序中可以有多个VBO）获取则是通过在调用glVetexAttribPointer时绑定到GL_ARRAY_BUFFER的VBO决定的。
 	//由于在调用glVetexAttribPointer之前绑定的是先前定义的VBO对象，顶点属性0现在会链接到它的顶点数据。
 
+	glEnableVertexAttribArray(0);
 
-	ddddddddddddddd
-	sddfkjdkfjsd
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+
+	// uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
 	
 	
 	
 	while (!glfwWindowShouldClose(window)) {
-	
-		
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glBindVertexArray(0); // no need to unbind it every time 
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
+
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 
 }
 
 
-
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 void framerbuffer_size_callback(GLFWwindow * window, int width, int height) {
 	glViewport(0, 0, width, height);
