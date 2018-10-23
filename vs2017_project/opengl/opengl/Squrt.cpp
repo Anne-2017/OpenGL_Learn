8deca2a5ac2fa4c69be14b3ca1fb4ea52e530d3f
q313923011 <maksimtianblue@hotmail.com>
2018-10-23 21:25:21 +0800

    v'v'v

--- vs2017_project/opengl/opengl/Squrt.cpp
+++ vs2017_project/opengl/opengl/Squrt.cpp
@@ -33,7 +33,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//创建窗体
-	GLFWwindow*window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"Traingle", NULL, NULL);
+	GLFWwindow*window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Traingle", NULL, NULL);
 	if (window == NULL) {
 		cout << "fail to creat glfw window" << endl;
 		glfwTerminate();
@@ -41,7 +41,7 @@ int main() {
 	}
 
 	//创建上下文
-	glfwMakeContextCurrent(window);	
+	glfwMakeContextCurrent(window);
 	//回调函数 窗口大小
 	glfwSetFramebufferSizeCallback(window, framerbuffer_size_callback);
 
@@ -65,7 +65,7 @@ int main() {
 	char infoLog[512]; //字符串
 
 	//用glGetShaderiv检查是否编译成功
-	glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
 		//glGetShaderInfoLog 会获取 glGetShaderiv的信息
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
@@ -101,10 +101,10 @@ int main() {
 
 
 	float vertices[] = {
-		// 第一个三角形
-	 0.5f, 0.5f, 0.0f,   // 右上角
-	 0.5f, -0.5f, 0.0f,  // 右下角
-	 -0.5f, 0.5f, 0.0f,  // 左上角
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+	   -0.5f, -0.5f, 0.0f,  // bottom left
+	   -0.5f,  0.5f, 0.0f   // top left 
 	};
 
 	//定义四边形 使用
@@ -112,28 +112,31 @@ int main() {
 		0, 1, 3,  // first Triangle
 		1, 2, 3   // second Triangle
 	};
-	
-     //http://www.opengl-tutorial.org/cn/intermediate-tutorials/tutorial-9-vbo-indexing/
-	//顶点数组对象(Vertex Array Object, VAO)可以像顶点缓冲对象那样被绑定，任何随后的顶点属性调用都会储存在这个VAO中。
-	unsigned int VBO, VAO;
+
+	//http://www.opengl-tutorial.org/cn/intermediate-tutorials/tutorial-9-vbo-indexing/
+   //顶点数组对象(Vertex Array Object, VAO)可以像顶点缓冲对象那样被绑定，任何随后的顶点属性调用都会储存在这个VAO中。
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);//new
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
-	// 0. 复制顶点数组到缓冲中供OpenGL使用 //GL_ELEMENT_ARRAY_BUFFER 索引绘图必须使用这个缓存对象。只能使用一个索引，而公共的顶点会复制一份 类似obj
+	// 2. 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用 //GL_ELEMENT_ARRAY_BUFFER 索引绘图必须使用这个缓存对象。只能使用一个索引，而公共的顶点会复制一份 类似obj
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	// 3. 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//new
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); //new
 
 	//使用glVertexAttribPointer函数告诉OpenGL该如何解析顶点数据（应用到逐个顶点属性上） . 设置顶点属性指针
-	glVertexAttribPointer(0 , 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 	//第一个参数指定我们要配置的顶点属性。还记得我们在顶点着色器中使用layout(location = 0)定义了position顶点属性的位置值(Location)吗？它可以把顶点属性的位置值设置为0。因为我们希望把数据传递到这一个顶点属性中，所以这里我们传入0。
 	//第二个参数指定顶点属性的大小。顶点属性是一个vec3，它由3个值组成，所以大小是3。
 	//第三个参数指定数据的类型，这里是GL_FLOAT(GLSL中vec*都是由浮点数值组成的)。
 	//第四个参数参数定义我们是否希望数据被标准化(Normalize)。如果我们设置为GL_TRUE，所有数据都会被映射到0（对于有符号型signed数据是 - 1）到1之间。我们把它设置为GL_FALSE。
 	//第五个参数叫做步长(Stride)，它告诉我们在连续的顶点属性组之间的间隔。由于下个组位置数据在3个float之后，我们把步长设置为3 * sizeof(float)。要注意的是由于我们知道这个数组是紧密排列的
-    //（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。一旦我们有更多的顶点属性，我们就必须更小心地定义每个顶点属性之间的间隔，我们在后面会看到更多的例子（译注: 这个参数的意思简单说就是从这个属性第二次出现的地方到整个数组0位置之间有多少字节）
-    //最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。我们会在后面详细解释这个参数。
+	//（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。一旦我们有更多的顶点属性，我们就必须更小心地定义每个顶点属性之间的间隔，我们在后面会看到更多的例子（译注: 这个参数的意思简单说就是从这个属性第二次出现的地方到整个数组0位置之间有多少字节）
+	//最后一个参数的类型是void*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。我们会在后面详细解释这个参数。
 	//每个顶点属性从一个VBO管理的内存中获得它的数据，而具体是从哪个VBO（程序中可以有多个VBO）获取则是通过在调用glVetexAttribPointer时绑定到GL_ARRAY_BUFFER的VBO决定的。
 	//由于在调用glVetexAttribPointer之前绑定的是先前定义的VBO对象，顶点属性0现在会链接到它的顶点数据。
 
@@ -145,13 +148,22 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
-	// uncomment this call to draw in wireframe polygons.
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	
-	
-	
+
+
+
+//线框模式(Wireframe Mode)
+
+//要想用线框模式绘制你的三角形，你可以通过glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)函数配置OpenGL如何绘制图元。
+//第一个参数表示我们打算将其应用到所有的三角形的正面和背面，第二个参数告诉我们用线来绘制。之后的绘制调用会一直以线框模式绘制三角形，
+//直到我们用glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)将其设置回默认模式。
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -160,7 +172,8 @@ int main() {
 		// draw our first triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //new
 		// glBindVertexArray(0); // no need to unbind it every time 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -192,3 +205,5 @@ void processInput(GLFWwindow *window)
 void framerbuffer_size_callback(GLFWwindow * window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
+
+
